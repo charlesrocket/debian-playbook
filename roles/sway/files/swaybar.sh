d106eaeeb0da6c57d3f9328a0a35d52451ed9956
-k <slowdive@me.com>
2020-11-13 16:38:06 -0800

    Update swaybar.sh

--- roles/sway/files/swaybar.sh
+++ roles/sway/files/swaybar.sh
@@ -6,21 +6,20 @@ battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "
 battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
 ##
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
-ping=$(ping -c 1 www.google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 loadavg_5=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 ##
 if [ $battery_status = "discharging" ];
 then
-    battery_pluggedin='⚠'
+    battery_pluggedin='🔋'
 else
     battery_pluggedin='⚡'
 fi
 
 if ! [ $network ]
 then
-   network_active="⛔"
+   network_active="✘"
 else
    network_active="⇆"
 fi
 ##
-echo "$network_active ($ping ms) | $loadavg_5 | $battery_pluggedin $battery_charge | $date $time"
+echo "$network_active | $loadavg_5 | $battery_pluggedin $battery_charge | $date $time"
